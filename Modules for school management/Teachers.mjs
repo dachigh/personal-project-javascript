1b4406c806b52b74a8f13567b247c9cd90bba37a
dachigh <d_ghoniadze@cu.edu.ge>
2022-06-27 05:48:39 +0400

    Added some method:read,update,remove

--- Modules for school management/Teachers.mjs
+++ Modules for school management/Teachers.mjs
@@ -12,6 +12,35 @@ export class Teachers{
             return id;
         }
 
+        read(id){
+            if (typeof id !== 'string'){
+                throw new TypeError("Teachers_ID must be string type AND is required");
+            }
+            if (id.length === 0){
+                throw new TypeError("Teachers_ID must not be EMPTY string");
+            }
+            return this.#teachers.has(id)? {id,...this.#teachers.get(id)} : null;
+        }
+
+
+        update(id, updatedTeacher){
+            validateTeachers(updatedTeacher);
+            const foundTeacher = this.read(id);
+            delete foundTeacher.id;
+            this.#teachers.set(id, {
+                ...foundTeacher,
+                ...updatedTeacher
+            });
+        }
+
+        remove(id){
+            if(!this.#teachers.has(id)){
+                throw new Error('Doesn\'t exist this Teacher')
+            }
+            this.#teachers.delete(id);
+        }
+
+
 
 }
 
